3949c820242736c9a712c893c620b47483af09be
Peng <pengzhang@falcon-computing.com>
2020-01-27 17:24:48 -0500

    add bit=8

--- bin/run_enc.sh
+++ bin/run_enc.sh
@@ -1,2 +1,2 @@
-./app_encoder --config ../cfg/encode_IPPP.cfg -v 2 -i akiyo_qcif.yuv -w 176 -h 144 -z 30 -o akiyo_qcif.enc -r akiyo_qcif.rec
+./app_encoder --config ../cfg/encode_IPPP.cfg -v 2 -i akiyo_qcif.yuv -w 176 -h 144 -z 30 -d 8 -f 9 -o akiyo_qcif.enc -r akiyo_qcif.rec
 diff akiyo_qcif.enc akiyo_qcif.enc.ref && echo "Success!"
